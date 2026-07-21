5101536c1c42ad2534b69d3a5a1b6b9b0ea9f8e5
andrew28r <andrewrothschild28@gmail.com>
2026-07-21 15:41:23 -0400

    send

--- js/game.js
+++ js/game.js
@@ -853,6 +853,7 @@ document.getElementById("testGameBtn").addEventListener("click", () => {
   window.location.href = `game.html?date=${nextDate}`;
 });
 
+
 function formatLocalDate(date) {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
