c82144ef416e042c69ec1651debabff9c554e508
andrew28r <andrewrothschild28@gmail.com>
2026-07-28 22:35:38 -0400

    PWA setup no cache

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
 const CACHE_NAME = "daily-diamond-v1";
 
 const STATIC_FILES = [
-    "./",
-    "./index.html",
-    "./game.html",
-    "./ratings.html",
-    "./css/style.css",
-    "./js/index.js",
-    "./js/game.js",
-    "./js/database.js",
-    "./js/api.js",
-    "./js/rating.js",
     "./icon-192.png",
     "./icon-512.png"
 ];
 
-
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(STATIC_FILES))
+            .then(cache => cache.addAll(STATIC_FILES))
+    );
+
+    self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        self.clients.claim()
     );
 });
 
@@ -27,6 +25,6 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         fetch(event.request)
-        .catch(() => caches.match(event.request))
+            .catch(() => caches.match(event.request))
     );
 });
